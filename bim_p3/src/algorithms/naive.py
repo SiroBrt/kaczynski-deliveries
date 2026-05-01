f5732711be0253b157aa49272a20c34513d3677b
dubi du <100496683@alumnos.uc3m.es>
2026-05-01 21:18:56 +0200

    from 0 to hero

--- bim_p3/src/algorithms/naive.py
+++ bim_p3/src/algorithms/naive.py
@@ -1,18 +1,139 @@
+import random
 try:
     from ..evaluation import evaluate_solution
-    from ..problem import ProblemInstance
+    from ..problem import ProblemInstance, Customer
+    from baseline import greedy_baseline
 except ImportError:
     from evaluation import evaluate_solution
-    from problem import ProblemInstance
+    from problem import ProblemInstance, Customer
+    from algorithms.baseline import greedy_baseline
 
 
+def find_route(
+    old_problem: ProblemInstance,
+    nodes:[Customer]
+    ):
+    new_problem = ProblemInstance(
+        depot_x=old_problem.depot_x,
+        depot_y=old_problem.depot_y,
+        customers=nodes,
+        vehicle_capacity=old_problem.vehicle_capacity,
+        num_vehicles=old_problem.num_vehicles,
+        vehicle_speed=old_problem.vehicle_speed,
+    )
+    fake_result = greedy_baseline(new_problem) 
+    # this actually uses de position of the customers in the list instead of the idx attribute, so we have to convert it
+    result = []
+    new_cust = []
+    for route in fake_result:
+        result.append([])
+        for i in route:
+            result[-1].append(nodes[i-1].idx)
+    return result
+
+# OBSOLETE
+
+# def choose_clients_greedy(problem: ProblemInstance):
+#     not_included_clients = [c for c in problem.customers]
+#     included_clients = []
+#     included_eval = evaluate_solution(problem,[[]]).total_cost
+#     for number_served in range(len(problem.customers)):
+#         current_best = float("inf")
+#         to_add = not_included_clients[0]
+#         for i in not_included_clients:
+#             possible_clients = [c for c in included_clients] + [i]
+#             possible_evaluation =  evaluate_solution(
+#                     problem,
+#                     find_route(problem,possible_clients)
+#                     ).total_cost
+#             if possible_evaluation <= current_best:
+#                 to_add = i
+#                 current_best = possible_evaluation
+#         if current_best <= included_eval:
+#             included_clients.append(to_add)
+#             included_eval = current_best
+#             not_included_clients.remove(to_add)
+#         else:
+#             break
+#     result = find_route(problem,included_clients)
+#     return find_route(problem,included_clients)
+#
+# def choose_clients_counter_greedy(problem: ProblemInstance):
+#     included_clients = [c for c in problem.customers]
+#     included_eval = evaluate_solution(problem,[[]]).total_cost
+#     for number_served in range(len(problem.customers),0,-1):
+#         current_best = float("inf")
+#         to_remove = included_clients[0]
+#         for i in included_clients:
+#             possible_clients = [j for j in included_clients]
+#             possible_clients.remove(i)
+#             possible_evaluation =  evaluate_solution(
+#                     problem,
+#                     find_route(problem,possible_clients)
+#                     ).total_cost
+#             if possible_evaluation <= current_best:
+#                 to_remove = i
+#                 current_best = possible_evaluation
+#         if current_best <= included_eval:
+#             included_clients.remove(to_remove)
+#             included_eval = current_best
+#         else:
+#             break
+#     result = find_route(problem,included_clients)
+#     return find_route(problem,included_clients)
+
+def choose_clients_alternating(problem: ProblemInstance, included_clients):
+    """
+    included_clients works as an initial guess for the best subset of customers.
+    We will then add or remove the most beneficial one at a time until we get to a minimum
+
+    Inspired by variable selection techinques in statistics
+    """
+    included_eval = evaluate_solution(problem,find_route(problem,included_clients)).total_cost
+    current_best = float("inf")
+    while True:
+        for i in problem.customers:
+            if i in included_clients:
+                possible_clients = [c for c in included_clients]
+                possible_clients.remove(i)
+            else:
+                possible_clients = [c for c in included_clients] + [i]
+            possible_evaluation = evaluate_solution(problem,find_route(problem,possible_clients)).total_cost
+            if possible_evaluation <= current_best:
+                to_change = i
+                current_best = possible_evaluation
+        if current_best < included_eval:
+            if to_change in included_clients:
+                included_clients.remove(to_change)
+            else:
+                included_clients.append(to_change)
+            included_eval = current_best
+        else:
+            break
+    return find_route(problem,included_clients)
+
+    
 def run_naive(
     problem: ProblemInstance,
     iterations: int = 60,
     seed: int = 0,
+    tries: int = 50
 ) -> tuple[list[list[int]], list[float]]:
 
-    return [[1,2]],[]
+    rutas = []
+    rutas.append(choose_clients_alternating(problem,[])) # gives a first solution
+    rutas.append(choose_clients_alternating(problem,[c for c in problem.customers])) # improves baseline
+    evals = []
+    evals.append(evaluate_solution(problem,rutas[0]).total_cost)
+    evals.append(evaluate_solution(problem,rutas[1]).total_cost)
+
+    # random starts to get different minima
+    for i in range(tries):
+        rutas.append(choose_clients_alternating(problem,random.sample(problem.customers,len(problem.customers)//2)))
+        evals.append(evaluate_solution(problem,rutas[-1]).total_cost)
+    return rutas[evals.index(min(evals))], []
+
+
 
 def run(problem: ProblemInstance, seed: int = 0) -> tuple[list[list[int]], list[float]]:
     """
